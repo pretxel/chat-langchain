5bfa78f0eaa17164ac0493766e1c4cd4a3735699
Edsel <pretxel@hotmail.com>
2023-10-08 16:22:22 +0100

    fix: metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,22 @@ export const metadata: Metadata = {
   description: "A basic chat app with a chatbot powered by langchain",
   keywords: ["chat, react, nextjs, ai, langchain"],
   publisher: "Edsel Serrano",
+  metadataBase: new URL("https://chat.edselserrano.com"),
   openGraph: {
     images: "/og-image.png",
+  },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
   },
 };
 
